b570f47881f725dac23ef04536f3141ad968a0ed
Radimir Mignogna <radimirmignogna@MacBook-Air-di-Radimir-2.local>
2026-08-21 09:30:44 +0200

    Added print_node_list() to be able to print the value stored in the graph's nodes, and did a simulation of adding a new animal Classification

--- main.c
+++ main.c
@@ -1,13 +1,78 @@
 #include <stdio.h>
 #include "zooManager.h"
+#include "directed_graph.h"
 
 typedef enum {SIMBA, RAJAH, GALENA, RIO, KAA, SHELLY} animali;
 const char* animal_name[] = {"Simba", "Rajah", "Galena", "Rio", "Kaa", "Shelly"};
 
+
+// ####### TURN print_node_list GENERIC #########
+
+void print_node_list(direct_graph _g, const list _list, void(*print_value)(void*)) {
+    iterator it = list_iterator_create(_list);
+
+    while (iterator_has_next(it)) {
+        int* id = (int*)iterator_next(it);
+        void* value;
+
+        if (get_value(_g, *id, &value) == DIRECT_GRAPH_SUCCESS) {
+            print_value(value);
+            printf(" ");
+        }
+    }
+    iterator_destroy(&it);
+    printf("\n");
+}
+
+void print_string(void* value) {
+    printf("%s ", (char*)value);
+}
+
 int main() {
+    direct_graph grafoClassificazione = direct_graph_create(200);
+
+    // Categoria generale
+    node_id classificazione = add_node(grafoClassificazione, "Classificazione");
+    node_id Mammiferi = add_node(grafoClassificazione, "Mammiferi");
+    node_id Rettili = add_node(grafoClassificazione, "Rettili");
+    node_id Uccelli = add_node(grafoClassificazione, "Uccelli");
+
+    add_edge(grafoClassificazione, classificazione, Mammiferi);
+    add_edge(grafoClassificazione, classificazione, Rettili);
+    add_edge(grafoClassificazione, classificazione, Uccelli);
 
 
+    // Felidi
+    node_id Felidi = add_node(grafoClassificazione, "Felidi");
+    node_id leone = add_node(grafoClassificazione, "leone");
+    node_id tigre = add_node(grafoClassificazione, "tigre");
 
+    add_edge(grafoClassificazione, Mammiferi, Felidi);
+    add_edge(grafoClassificazione, Felidi, leone);
+    add_edge(grafoClassificazione, Felidi, tigre);
+
+    // Giraffidi
+    node_id Giraffidi = add_node(grafoClassificazione, "Giraffidi");
+    node_id giraffa = add_node(grafoClassificazione, "giraffa");
+
+    add_edge(grafoClassificazione, Mammiferi, Giraffidi);
+    add_edge(grafoClassificazione, Giraffidi, giraffa);
+
+
+    /*
+    printf("DFS from node n1: ");
+    list _dfs = direct_graph_dfs(grafoClassificazione, classificazione);
+    print_node_list(grafoClassificazione, _dfs, print_string);
+    list_destroy(&_dfs);
+    */
+
+    printf("BFS from node n1: ");
+    list _bfs = direct_graph_bfs(grafoClassificazione, classificazione);
+    print_node_list(grafoClassificazione, _bfs, print_string);
+    list_destroy(&_bfs);
+
+
+/*
     // Animals hashmap test
     hashmap hashAnimals = hashmap_create(10);
 
@@ -31,7 +96,7 @@ int main() {
 
     // Areas hashmap test
 
-
+*/
 
     return 0;
 }
